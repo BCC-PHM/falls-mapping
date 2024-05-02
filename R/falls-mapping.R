44910eed55dcf3237745b510cb79cfe8f9a736b9
David.Ellis <david.ellis@birmingham.gov.uk>
2024-05-02 11:54:18 +0100

    Added mapping for raw number of falls

--- R/falls-mapping.R
+++ R/falls-mapping.R
@@ -35,7 +35,8 @@ ward_falls <- convert_GP_data(
   norm_output_per = 1000
 ) %>%
   mutate(
-    `Falls per 1000 patients aged 65+` = `number_of_falls per 1000 Patients65Plus`
+    `Falls per 1000 patients aged 65+` = `number_of_falls per 1000 Patients65Plus`,
+    `Number of falls` = number_of_falls
   )
 
 palette <- ggpubr::get_palette((c("#FFFFFF", "#105ca5")), 20)
@@ -52,5 +53,26 @@ map <- plot_map(
 )
 map
 
+save_map(map, save_name = "../output/BSol-falls-22-23.png",
+         width = 4.5, height = 6)
 save_map(map, save_name = "../output/BSol-falls-22-23.html",
+         width = 4.5, height = 6)
+
+
+# Plot BSol map
+raw_falls_map <- plot_map(
+  ward_falls,
+  value_header = "Number of falls",
+  map_type = "Ward",
+  area_name = "Birmingham",
+  map_title = "Emergency hospital admissions for falls injuries in persons aged 65 and over (2022/23)",
+  style = "cont",
+  #breaks = c(0, 10, 20, 30),
+  palette = palette
+)
+raw_falls_map
+
+save_map(raw_falls_map, save_name = "../output/BSol-falls-raw-22-23.png",
+         width = 4.5, height = 6)
+save_map(raw_falls_map, save_name = "../output/BSol-falls-raw-22-23.html",
          width = 4.5, height = 6)
